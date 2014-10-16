a035d15f22edfb4bd209f4d30832937c937ede75
pascal <pascal.carrie@hola-internet.com>
2014-10-16 14:47:21 +0200

    stucturing the test

--- askar/functional_tests.py
+++ askar/functional_tests.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+  
+  def setUp(self):
+    self.browser = webdriver.Firefox()
+    self.browser.implicitly_wait(3)
+    self.browser.maximize_window()
 
-assert 'Django' in browser.title
+  def tearDown(self):
+    self.browser.quit()
+
+  # on the home page
+  def testroot_page_prepare_client(self):
+    self.browser.get('http://localhost:8000')
+
+    # the title is set
+    self.assertIn('Askar', self.browser.title)
+    
+    self.fail('Finish the test')
+
+if __name__ == '__main__':
+  unittest.main(warnings='ignore')
